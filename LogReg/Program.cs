a4e3083d3260ec3c3bd4f9618b12081d655db729
Semyon Suvorov <semen.suvoroff@yandex.ru>
2024-01-16 17:43:02 +0300

    Tokenizer

--- LogReg/Program.cs
+++ LogReg/Program.cs
@@ -1,10 +1,10 @@
 ﻿using ClassLibrary;
 using Deedle;
-
+using TextPreprocessing;
 using Frame = Deedle.Frame;
 
 
-
+/*
 Frame<int, string> frame = Frame.ReadCsv("simple_data_for_linear_regression.csv");
 var trainRowIndices = new int[300];
 for (int i = 0; i < 300; i++)
@@ -25,8 +25,26 @@ var testFrame = frame.GetRows(testRowIndices);
 var yTrue = testFrame.GetColumn<string>("Target").Select(x => x.Value).Values.ToList();
 var logreg = new LogisticRegression(10000);
 logreg.Fit(trainFrame, 0.01);
+
 var preds = logreg.PredictProbaForMultipleSamples(testFrame);
 for(int i = 0; i < preds.Count; i++)
 {
     Console.WriteLine($"Predicted proba: {preds[i]} -> Real label: {yTrue[i]}");
 }
+
+var oneSample = new List<double>()
+{
+    365.0, -1.0978727092313199, 0.5766048932140662,
+    -0.0030922379575568136, 0.5469982296454587, -1.0001157199466832, 0.2471083764924692,
+    -0.4616078693750237, -0.028045418922321503, 0.5310816568918002
+}; //1
+var predForOneSample = logreg.PredictProbaForOneSample(oneSample);
+Console.WriteLine(predForOneSample);
+*/
+
+
+var p = new PreprocessText();
+
+var toyFrame = Frame.ReadCsv("test.csv");
+p.TokenizeTextCorpus(toyFrame);
+p.PrintDict();
---
+++ TextPreprocessing/PreprocessText.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.RegularExpressions;
+using Deedle;
+
+
+namespace TextPreprocessing;
+
+public class PreprocessText
+{
+    private Dictionary<string, int> _dictOfWords = new ();
+
+    private readonly List<string> _stopWordsList = new();
+
+    public PreprocessText()
+    {
+        const string filePath = "stopwords.txt";
+        var stream = new FileStream(filePath, FileMode.Open);
+        var reader = new StreamReader(stream);
+        var parts = reader.ReadLine()?.Split(',');
+
+        if (parts != null)
+            foreach (var t in parts)
+            {
+                _stopWordsList.Add(t);
+            }
+
+        reader.Close();
+    }
+    
+    private void TokenizeIntoWords(string input)
+    {
+        var tokens = input.Split(" ").ToList();
+        foreach (var token in tokens)
+        {
+            var loweredToken = token.ToLower();
+            var normalizedToken = Regex.Replace(loweredToken, 
+                "[-.?!)(,:0123456789/\t\n$%^*}{#@<>'['~;@]", "")
+                .Replace(@"\", "").Replace("\"","");
+            if (_stopWordsList.Contains(normalizedToken))
+            {
+                continue;
+            }
+            
+            if (_dictOfWords.ContainsKey(normalizedToken))
+            {
+                _dictOfWords[normalizedToken]++;
+            }
+            
+            _dictOfWords.TryAdd(normalizedToken, 1);
+        }
+    }
+
+    public void TokenizeTextCorpus(Frame<int, string> df)
+    {
+        var header = df.GetColumn<string>("Header").Values.ToList();
+        var description = df.GetColumn<string>("Description").Values.ToList();
+        var concatedFeature = new string[header.Count];
+        for (int i = 0; i < header.Count; i++)
+        {
+            concatedFeature[i] = header[i] + " " + description[i];
+        }
+
+        foreach (var feature in concatedFeature)
+        {
+            TokenizeIntoWords(feature);
+        }
+    }
+
+    public void PrintDict()
+    {
+        foreach (var pair in _dictOfWords.OrderByDescending(pair => pair.Value))
+        {
+            Console.WriteLine($"{pair.Key} -> {pair.Value}");
+        }
+    }
+}
